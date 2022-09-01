c7ad42467342030eb97bd545590450dda4924fd8
kasradara <kasradara@outlook.com>
2022-09-01 17:03:43 +0430

    feat: input component -wip

---
+++ components/form/Input.vue
@@ -0,0 +1,45 @@
+<template>
+  <b-form-group :label="label" :label-for="inputId">
+    <b-form-input
+      :id="inputId"
+      v-model="model"
+      :type="type"
+    />
+    <b-form-invalid-feedback v-text="'salam'" />
+  </b-form-group>
+</template>
+
+<script>
+export default {
+  name: 'InputComponents',
+  props: {
+    value: {
+      type: String,
+      required: true
+    },
+    inputId: {
+      type: String,
+      required: true
+    },
+    label: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      default: 'text'
+    }
+  },
+  computed: {
+    model: {
+      get () {
+        return this.value
+      },
+      set (value) {
+        this.$emit('input', value)
+      }
+    }
+  }
+
+}
+</script>
