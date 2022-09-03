67980abefc422a3d78470a557ad93c9f57ff08c9
kasradara <kasradara@outlook.com>
2022-09-03 15:22:26 +0430

    feat: implement layout -wip

---
+++ layouts/panel.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="panel-layout">
+    <div class="nav bg-dark text-light d-flex justify-content-between align-items-center py-2 px-4">
+      <div>
+        <span class="title"> Arvan Challenge</span>
+        <span v-if="user.username"> Welcome {{ user.username }}</span>
+      </div>
+      <b-button variant="outline-info">
+        Logout
+      </b-button>
+    </div>
+    <div>
+      <div class="sidebar bg-primary">
+        e
+      </div>
+      <div class="main-section">
+        <b-container fluid>
+          <Nuxt />
+        </b-container>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PanelLayout',
+  computed: {
+    user () {
+      return this.$store.state.user.user
+    }
+  }
+}
+</script>
+<style lang="scss">
+  .panel-layout{
+    .nav{
+      position: sticky;
+      top: 0;
+      .title{
+        font-size: 22px;
+        margin-right: 20px;
+      }
+    }
+    .sidebar{
+      width: 250px;
+      height: 100%;
+      position: fixed;
+    }
+    .main-section{
+      margin-left: 250px;
+    }
+
+  }
+</style>
