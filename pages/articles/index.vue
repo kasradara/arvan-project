341b713317192ab7814fc6fb61253ea1526345ce
kasradara <kasradara@outlook.com>
2022-09-05 13:54:31 +0430

    feat: delete article added

--- pages/articles/index.vue
+++ pages/articles/index.vue
@@ -1,45 +1,55 @@
 <template>
   <div>
-    <h1>All Posts</h1>
-    <div class="table-box">
-      <table>
-        <tr class="header">
-          <th>#</th>
-          <th>Title</th>
-          <th>Author</th>
-          <th>Tags</th>
-          <th>Excerpt</th>
-          <th>Created</th>
-          <th />
-        </tr>
-        <tr v-for="(article, index) in articles" :key="index" class="body">
-          <td>{{ index+1 }}</td>
-          <td>{{ article.title }}</td>
-          <td>@{{ article.author.username }}</td>
-          <td>
-            <span v-for="tag in article.tagList" :key="tag" class="d-block">
-              {{ tag }}
-            </span>
-          </td>
-          <td>{{ article.description | strLimit(20) }}</td>
-          <td>{{ article.createdAt | fullDate }}</td>
-          <td>
-            <b-dropdown variant="info" right>
-              <nuxt-link tag="b-dropdown-item" :to="`/articles/edit/${article.slug}`">
-                tset
-              </nuxt-link>
-              <b-dropdown-item>Item 2</b-dropdown-item>
-            </b-dropdown>
-          </td>
-        </tr>
-      </table>
+    <ALoading v-if="$fetchState.pending" />
+    <div v-else>
+      <h1 class="py-4">
+        All Posts
+      </h1>
+      <div class="table-box">
+        <table>
+          <tr class="header">
+            <th>#</th>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Tags</th>
+            <th>Excerpt</th>
+            <th>Created</th>
+            <th />
+          </tr>
+          <tr v-for="(article, index) in articles" :key="index" class="body">
+            <td>{{ index+1 }}</td>
+            <td>{{ article.title }}</td>
+            <td>@{{ article.author.username }}</td>
+            <td>
+              <span v-for="tag in article.tagList" :key="tag" class="d-block">
+                {{ tag }}
+              </span>
+            </td>
+            <td>{{ article.description | strLimit(20) }}</td>
+            <td>{{ article.createdAt | fullDate }}</td>
+            <td>
+              <b-dropdown variant="info" right>
+                <nuxt-link tag="b-dropdown-item" :to="`/articles/edit/${article.slug}`">
+                  edit
+                </nuxt-link>
+                <b-dropdown-item @click="deleteArticle(article.slug)">
+                  delete
+                </b-dropdown-item>
+              </b-dropdown>
+            </td>
+          </tr>
+        </table>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import ALoading from '@/components/partials/Loading.vue'
+
 export default {
   name: 'ArticlesPage',
+  components: { ALoading },
   layout: 'panel',
   data () {
     return {
@@ -47,8 +57,19 @@ export default {
     }
   },
   async fetch () {
-    const { data } = await this.$axios.get('api/articles')
+    const { data } = await this.$axios.get('api/articles', {
+      params: { limit: 4, offset: 3 }
+    })
     this.articles = data.articles
+  },
+  methods: {
+    deleteArticle (slug) {
+      this.$axios
+        .$delete(`api/articles/${slug}`)
+        .then(() => {
+          this.$fetch()
+        })
+    }
   }
 }
 </script>
