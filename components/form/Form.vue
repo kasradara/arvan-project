bd50aee4fed425cc3e6ebfb7c3e58aa043b0f746
kasradara <kasradara@outlook.com>
2022-09-05 13:52:36 +0430

    feat : add form component

---
+++ components/form/Form.vue
@@ -0,0 +1,19 @@
+<template>
+  <ValidationObserver ref="form" v-slot="{ handleSubmit }">
+    <b-form @submit.prevent="handleSubmit(request)">
+      <slot />
+    </b-form>
+  </ValidationObserver>
+</template>
+
+<script>
+export default {
+  name: 'FormValidation',
+  props: {
+    request: {
+      type: Function,
+      required: true
+    }
+  }
+}
+</script>
