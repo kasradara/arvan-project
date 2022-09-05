cc1b787ce08b3e0b4b88a888413c6f57a99cdf46
kasradara <kasradara@outlook.com>
2022-09-05 13:54:50 +0430

    feat: implement loading component

---
+++ components/partials/Loading.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="loading">
+    <b-spinner variant="primary" class="spinner" />
+    <span class="content text-primary">
+      Loading
+    </span>
+  </div>
+</template>
+<script>
+export default {
+  name: 'LoadingComponent'
+}
+</script>
+<style lang="scss" scoped>
+  .loading{
+    text-align: center;
+    .spinner{
+      width: 150px;
+      height: 150px;
+      margin-top: 150px;
+    }
+    .content{
+      display: block;
+      margin-top: 15px;
+      font-size: 20px;
+      font-weight: bold;
+    }
+  }
+</style>
