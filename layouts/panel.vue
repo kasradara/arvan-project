432adaa11d0304e82d655b4d10400e28a5376e86
kasradara <kasradara@outlook.com>
2022-09-05 15:04:53 +0430

    feat: panel layout done

--- layouts/panel.vue
+++ layouts/panel.vue
@@ -1,17 +1,35 @@
 <template>
   <div class="panel-layout">
-    <div class="nav bg-dark text-light d-flex justify-content-between align-items-center py-2 px-4">
+    <nav class="nav bg-dark text-light d-flex justify-content-between align-items-center py-2 px-4">
       <div>
         <span class="title"> Arvan Challenge</span>
         <span v-if="user.username"> Welcome {{ user.username }}</span>
       </div>
-      <b-button variant="outline-info">
+      <b-button variant="outline-info" @click="logout()">
         Logout
       </b-button>
-    </div>
+    </nav>
     <div>
       <div class="sidebar bg-primary">
-        e
+        <h3 class="sidebar-title">
+          Post
+        </h3>
+        <ul>
+          <nuxt-link :to="{name:'articles'}">
+            <li>
+              <span>
+                All Articles
+              </span>
+            </li>
+          </nuxt-link>
+          <nuxt-link :to="{name:'articles-create'}">
+            <li>
+              <span>
+                New Article
+              </span>
+            </li>
+          </nuxt-link>
+        </ul>
       </div>
       <div class="main-section">
         <b-container fluid>
@@ -29,6 +47,12 @@ export default {
     user () {
       return this.$store.state.user.user
     }
+  },
+  methods: {
+    logout () {
+      this.$cookies.remove('authToken')
+      this.$router.push({ name: 'login' })
+    }
   }
 }
 </script>
@@ -37,6 +61,7 @@ export default {
     .nav{
       position: sticky;
       top: 0;
+      z-index: 2;
       .title{
         font-size: 22px;
         margin-right: 20px;
@@ -46,6 +71,27 @@ export default {
       width: 250px;
       height: 100%;
       position: fixed;
+      color: white;
+      .sidebar-title{
+        padding: 16px 16px 8px;
+        font-size: 22px;
+      }
+      ul{
+        padding: 0;
+        list-style: none;
+        font-size: 18px;
+        a{
+          text-decoration: none;
+          color: white;
+        }
+        li{
+          padding: 12px 32px;
+        }
+        li:hover{
+          background: rgba(255, 255, 255, 0.15);
+          cursor: pointer;
+        }
+      }
     }
     .main-section{
       margin-left: 250px;
